5bac4175234b78db6e1f0387dd121acf898a339f
Pradeep K. Pant <ppant@cpan.org>
2016-08-23 11:03:20 +0530

    Added code redLines on text files

--- TestProgram.R
+++ TestProgram.R
@@ -22,5 +22,18 @@ if(!file.exists("./projectData/Coursera-SwiftKey.zip")){
 if(!file.exists("./projectData/final")){
   unzip(zipfile="./projectData/Coursera-SwiftKey.zip",exdir="./projectData")
 }
-
 # Once the dataset is downloaded start reading it as this a huge dataset so we'll read line by line only the amount of data needed
+# before doing that lets first list all the files in the directory
+## List all the files of /final/en_US Dataset folder
+path <- file.path("./projectData/final" , "en_US")
+files<-list.files(path, recursive=TRUE)
+# Lets make a file connection of the twitter data set
+con <- file("./projectData/final/en_US/en_US.twitter.txt", "r") 
+## Read the first line of text 
+line<-readLines(con, 1) 
+## Read the next line of text 
+line<-readLines(con, 1) 
+## Read in the next 5 lines of text 
+line<-readLines(con, 5) 
+## Close the connection handle when you are done
+close(con) 
